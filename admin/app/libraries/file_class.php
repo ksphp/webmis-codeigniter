6587cea483d1336ede38cdd86769bfff5cf2fa52
kingsoul <admin@ksphp.com>
2013-07-07 00:41:49 -0700

    file manager

--- admin/app/libraries/file_class.php
+++ admin/app/libraries/file_class.php
@@ -2,30 +2,6 @@
 class file_class{
 	var $file_root = '.';
 	
-	//上传
-	function upload() {
-		$targetFolder = '/upload'; // Relative to the root
-
-		$verifyToken = md5('unique_salt' . @$_POST['timestamp']);
-		
-		if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
-			$tempFile = $_FILES['Filedata']['tmp_name'];
-			$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
-			$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
-			
-			// Validate the file type
-			$fileTypes = array('jpg','jpeg','gif','png','zip'); // File extensions
-			$fileParts = pathinfo($_FILES['Filedata']['name']);
-			
-			if (in_array($fileParts['extension'],$fileTypes)) {
-				move_uploaded_file($tempFile,$targetFile);
-				echo '1';
-			} else {
-				echo 'Invalid file type.';
-			}
-		}
-	}
-	
 	//文件列表
 	function lists($c='.') {
 		//替换 “../”|“./”
