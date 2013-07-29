4b6b8aae45ba60f565aacdc0ea3e0e944a41e823
kingsoul <admin@ksphp.com>
2013-07-29 03:02:01 -0700

    WebMIS Install

---
+++ install/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="kingsoul" />
+	<title><?php echo $config['title'];?></title>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link href="install.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="body">
+	<div class="top">
+		<div class="top_logo">&nbsp;</div>
+		<span class="top_link">
+			<a href="http://www.ksphp.com" target="_black" >官方网站</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+			<a href="http://www.ksphp.com/document.html" target="_black" >WebMIS文档</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+			<a href="http://www.ksphp.com/helps.html" target="_black" >帮助</a>
+		</span>
+	</div>
+	<div class="ct">
+		<div class="ct_top"><b>安装说明</b></div>
+		<div class="ct_ct">安装条款</div>
+		<div class="ct_bt">
+			<span class="ct_bt_msg">提示：如果看不出“圆角框”，请升级或更换浏览器！</span>
+			<span class="ct_bt_an">
+				<a href="#" class="an1" >下一步</a>
+			</span>
+		</div>
+	</div>
+	<div class="copy">Built by KSPHP and the <a href="http://www.ksphp.com" >KSPHP.COM</a> community</div>
+</div>
+</body>
+</html>
