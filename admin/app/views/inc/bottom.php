c577d6a4d89392e1d379dfaed368271c0acb6d73
kingsoul <admin@ksphp.com>
2013-08-21 15:35:41 -0700

    Page bug

--- admin/app/views/inc/bottom.php
+++ admin/app/views/inc/bottom.php
@@ -8,10 +8,10 @@
 <div id="getUrl" style="display: none;"><?php echo @$get_url; ?></div>
 <div id="NavId" style="display: none;"><?php echo $NavId; ?></div>
 <div id="MenuTwoId" style="display: none;"><?php echo $MenuTwoId; ?></div>
-<script language="javascript" src="../webmis/plugin/jquery/jquery-1.10.2.min.js"></script>
-<script language="javascript" src="../webmis/plugin/Validform_v5.3.2_min.js"></script>
-<script language="javascript" src="../webmis/js/webmis.js"></script>
-<script language="javascript" src="../webmis/js/admin.js"></script>
+<script language="javascript" src="<?php echo base_url('../webmis/plugin/jquery/jquery-1.10.2.min.js');?>"></script>
+<script language="javascript" src="<?php echo base_url('../webmis/plugin/Validform_v5.3.2_min.js');?>"></script>
+<script language="javascript" src="<?php echo base_url('../webmis/js/webmis.js');?>"></script>
+<script language="javascript" src="<?php echo base_url('../webmis/js/admin.js');?>"></script>
 <?php if(@$js){ foreach($js as $val){ ?>
 <script language="javascript" src="<?php echo base_url($val); ?>"></script>
 <?php }}?>
