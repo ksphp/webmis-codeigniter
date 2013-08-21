2d2984f87279cd976c963b5a8ae10961db47f828
kingsoul <admin@ksphp.com>
2013-08-21 15:23:27 -0700

    Mobile

--- admin/app/config/user_agents.php
+++ admin/app/config/user_agents.php
@@ -124,7 +124,6 @@ $mobiles = array(
 					'spv'				=> "SPV",
 					'zte'				=> "ZTE",
 					'sendo'				=> "Sendo",
-					'huawei'				=> "HUAWEI",
 
 					// Operating Systems
 					'symbian'				=> "Symbian",
