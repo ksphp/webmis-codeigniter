ff0e55bd867bc10ec833636ddea62be486831404
kingsoul <admin@ksphp.com>
2013-08-22 20:37:09 -0700

    Mobile

--- admin/app/views/login_v_mo.php
+++ admin/app/views/login_v_mo.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
