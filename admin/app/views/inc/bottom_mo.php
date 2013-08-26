8d99e7ddf70a06d55f7a9bc5fd85d13993e2f537
kingsoul <admin@ksphp.com>
2013-08-25 20:42:51 -0700

    WebMIS 4.0

---
+++ admin/app/views/inc/bottom_mo.php
@@ -0,0 +1,11 @@
+
+</div>
+<div class="copy"><?php echo $config['copy'];?></div>
+<script language="javascript" src="<?php echo base_url('../webmis/plugin/jquery/jquery-1.10.2.min.js');?>"></script>
+<script language="javascript" src="<?php echo base_url('../webmis/plugin/Validform_v5.3.2_min.js');?>"></script>
+<script language="javascript" src="<?php echo base_url('../webmis/js/webmis_mo.js');?>"></script>
+<?php if(@$js){ foreach($js as $val){ ?>
+<script language="javascript" src="<?php echo base_url($val); ?>"></script>
+<?php }}?>
+</body>
+</html>
---
+++ admin/app/views/inc/top_mo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="kingsoul" />
+	<title>WebMIS 管理员控制台</title>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link href="../webmis/css/admin_mo.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="top">
+	<span class="cp"><b id="webmisVersion">WebMIS</b></span>
+	<span class="out"><a href="<?php echo base_url('index_c/loginOut.html');?>" >注销</a></span>
+	<span class="info"><a href="#"><?php echo $uinfo['uname']; ?></a>(<?php echo $uinfo['name']; ?>)</a></span>
+	
+</div>
+<div class="nav">
+	<span class="null">&nbsp;</span>
+	<span><a href="#" class="an2">首页</a></span>
+	<span><a href="#" class="an1">系统</a></span>
+</div>
+<div class="ct_body">
