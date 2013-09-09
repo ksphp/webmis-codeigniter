0483358a6468d5aea221046066b32045aef7dd05
kingsoul <admin@ksphp.com>
2013-09-08 22:50:22 -0700

    webmis 4.2 menu mo

--- admin/js/admin.mo.js
+++ admin/js/admin.mo.js
@@ -42,3 +42,60 @@ function menuOne(id){
 		return false;
 	});
 }
+/*
+** ******动作******
+*/
+/*删除*/
+function actionDel(dataUrl,targetUrl) {
+	var id = $('#listBG').webmis('GetInputID',{type:' '});
+	if(id!=' '){
+		$.webmis.win('open',{title:'删除',width:moWidth,height:140,content:'<div class="delData"><input type="submit" id="delSub" value="彻底删除" /></div>'});
+		$('#delSub').webmis('SubClass'); //按钮样式
+		//点击提交
+		$('#delSub').click(function(){
+			$.post($base_url+dataUrl,{'id':id},function(data){
+				if(data){
+					$.webmis.win('close');
+					var url = $('#getUrl').text();
+					$.webmis.win('open',{content:'<b class="green">删除成功</b>',target:targetUrl+url,AutoClose:3});
+				}else{
+					$.webmis.win('close');
+					$.webmis.win('open',{content:'<b class="red">删除失败</b>',AutoClose:3});
+				}
+			});
+		});
+	}else{
+		$.webmis.win('open',{content:'<b class="red">请选择！</b>',AutoClose:3});
+	}
+}
+/*审核*/
+function actionAudit(dataUrl,targetUrl) {
+	var id = $('#listBG').webmis('GetInputID',{type:' '});
+	if(id!=' '){
+		$.webmis.win('open',{title:'审核',width:moWidth,height:140,content:'<div class="delData"><input type="submit" id="auditSub1" value="通过" />&nbsp;&nbsp;<input type="submit" id="auditSub2" value="不通过" /></div>'});
+		$('#auditSub1,#auditSub2').webmis('SubClass'); //按钮样式
+		//通过
+		$('#auditSub1').click(function(){
+			auditData(id,'1');
+		});
+		//不通过
+		$('#auditSub2').click(function(){
+			auditData(id,'2');
+		});
+	}else{
+		$.webmis.win('open',{content:'<b class="red">请选择！</b>',AutoClose:3});
+	}
+	//提交数据
+	var auditData = function(id,state){
+		$.post($base_url+dataUrl,{'id':id,'state':state},function(data){
+			if(data){
+				$.webmis.win('close');
+				var url = $('#getUrl').text();
+				$.webmis.win('open',{content:'<b class="green">审核成功</b>',target:targetUrl+url,AutoClose:3});
+			}else{
+				$.webmis.win('close');
+				$.webmis.win('open',{content:'<b class="red">审核失败</b>',AutoClose:3});
+			}
+		});
+	}
+}
