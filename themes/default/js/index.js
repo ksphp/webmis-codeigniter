4f53c31d36796064a97cdc190f9f66f4f7eb46ec
kingsoul <admin@ksphp.com>
2013-11-05 17:23:39 -0800

    js

--- themes/default/js/index.js
+++ themes/default/js/index.js
@@ -2,7 +2,7 @@ $(function(){
 	//加载文件
 	$.webmis.inc({files:[
 		$webmis_plugin + 'slide/jquery.slides.min.js',	//广告插件
-		$webmis_plugin + 'slide/slide.js',	//广告插件
+		$webmis_plugin + 'slide/slide.js'	//广告插件
 	]});
 	//Index AD
 	$('#slides').slidesjs({ width: 1000,height: 320,play: {active: true,auto: true,interval: 5000}});
