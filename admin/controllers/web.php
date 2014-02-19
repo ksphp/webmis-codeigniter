9c32c7848843f2ef0aaa33cf4df859f37142a76e
kingsoul <admin@ksphp.com>
2014-02-19 01:05:38 -0800

    WebMIS 5.0

--- admin/controllers/web.php
+++ admin/controllers/web.php
@@ -2,7 +2,8 @@
 class Web extends MY_Controller {
 	/* Index */
 	public function index(){
-		$data['HostUrl'] = @$_SERVER['REQUEST_SCHEME'].'://'.@$_SERVER['SERVER_NAME'];
+		print_r($_SERVER);
+		$data['HostUrl'] = 'http://'.@$_SERVER['SERVER_NAME'];
 		$this->MyView('web/index_v',$data);
 	}
 }
