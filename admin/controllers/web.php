2684377e0339f0759c94ab1aeadc764a6a0e4000
kingsoul <admin@ksphp.com>
2014-02-19 01:06:52 -0800

    WebMIS 5.0

--- admin/controllers/web.php
+++ admin/controllers/web.php
@@ -2,7 +2,6 @@
 class Web extends MY_Controller {
 	/* Index */
 	public function index(){
-		print_r($_SERVER);
 		$data['HostUrl'] = 'http://'.@$_SERVER['SERVER_NAME'];
 		$this->MyView('web/index_v',$data);
 	}
