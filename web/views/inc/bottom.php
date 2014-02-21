047a12cbe7c8c46f845a5c5b2f3c09a32a9f7056
kingsoul <admin@ksphp.com>
2014-02-20 19:34:22 -0800

    Jquery

--- web/views/inc/bottom.php
+++ web/views/inc/bottom.php
@@ -11,7 +11,7 @@
 </div>
 <div id="base_url" style="display: none;"><?php echo base_url().$this->config->config['index_url']; ?></div>
 <div id="IsMobile" style="display: none;"><?php echo $IsMobile; ?></div>
-<script language="javascript" src="<?php echo base_url('webmis/plugin/jquery/jquery-1.10.2.min.js'); ?>"></script>
+<script language="javascript" src="<?php echo base_url('webmis/plugin/jquery/jquery-1.11.0.min.js'); ?>"></script>
 <script language="javascript" src="<?php echo base_url('webmis/jquery.webmis.js'); ?>"></script>
 <script language="javascript" src="<?php echo base_url('themes/default/js/web.js'); ?>"></script>
 <?php if(@$js){ foreach($js as $val){ ?>
