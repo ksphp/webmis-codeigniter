b620d756feb8d82ca44c26efac736b04c0ca0fd7
kingsoul <admin@ksphp.com>
2014-05-09 00:08:24 +0800

    Highcharts

--- admin/controllers/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Web extends MY_Controller {
-	/* Index */
-	public function index(){
-		$data['HostUrl'] = 'http://'.@$_SERVER['SERVER_NAME'];
-		$this->MyView('web/index_v',$data);
-	}
-}
-?>
