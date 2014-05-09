f401134f2c1f6e7b77898caa3aa46c4830f8f7bf
kingsoul <admin@ksphp.com>
2014-05-09 09:54:14 +0800

    update

--- admin/controllers/system.php
+++
@@ -1,8 +0,0 @@
-<?php
-class System extends MY_Controller {
-	/* Index */
-	public function index(){
-		$this->MyView('system/index_v');
-	}
-}
-?>
--- admin/controllers/system_help.php
+++
@@ -1,8 +0,0 @@
-<?php
-class System_help extends MY_Controller {
-	/* Index */
-	public function index(){
-		$this->MyView('help/system_help_v');
-	}
-}
-?>
