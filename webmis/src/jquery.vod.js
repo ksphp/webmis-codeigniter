760111c9cafc5307394af0d92a5c5a60c9e68b88
kingsoul <admin@ksphp.com>
2014-05-28 11:54:21 +0800

    vod bug

--- webmis/src/jquery.vod.js
+++ webmis/src/jquery.vod.js
@@ -2,12 +2,13 @@
 var openVod = function (options) {
 	var defaults = {
 		title:'Html5 视频播放器',
-		src:'http://www.ksphp.com/vod/test.mp4',
+		src:'http://www.ksphp.com/upload/vod/test.mp4',
+		poster:'http://www.ksphp.com/upload/vod/test.jpg',
 		width:640,
 		height:390,
-		controls:true,
-		preload:true,
-		autoplay:true,
+		controls:'controls',
+		preload:'preload',
+		autoplay:'',
 		AlphaBG:0.5
 	}
 	var options = $.extend(defaults, options);
@@ -17,7 +18,7 @@ var openVod = function (options) {
 		var vh = (h - options.height - 30)/2;
 		var html = '<section id="WebMisVodBg" style="height: '+h+'px; background-color: rgba(0,0,0,'+options.AlphaBG+');"><section id="WebMisVodCT" style="width: '+options.width+'px; margin-top: '+vh+'px"><div class="display">';
 		html += '<header class="WebMisVodTop"><span class="title">'+options.title+'</span><em class="close">&nbsp;</em></header>';
-		html += '<video src="'+options.src+'" width="100%" height="'+options.height+'" controls="'+options.controls+'" preload="'+options.preload+'" autoplay="'+options.autoplay+'"></video>';
+		html += '<video src="'+options.src+'" poster="'+options.poster+'" width="100%" height="'+options.height+'" '+options.controls+' '+options.preload+' '+options.autoplay+'></video>';
 		html += '</div></section></section>';
 		$('body').prepend(html);
 		$('#WebMisVodBg').fadeIn();
