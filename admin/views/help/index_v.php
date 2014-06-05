8229b8999a187701407a84b4524253ce8edfdc88
kingsoul <admin@ksphp.com>
2014-06-05 19:57:45 +0800

    Help

--- admin/views/help/index_v.php
+++ admin/views/help/index_v.php
@@ -8,9 +8,8 @@
 	<tbody id="listBG1">
 	<tr>
 		<td class="text">
-			<p><b>帮助文档:</b>&nbsp;&nbsp;<a href="http://www.ksphp.com/document.html">http://www.ksphp.com/document.html</a></p>
+			<p><b>帮助文档:</b>&nbsp;&nbsp;<a href="http://www.ksphp.com/">http://www.ksphp.com/</a></p>
 			<p><b>项目地址:</b>&nbsp;&nbsp;<a href="https://github.com/ksphp/webmis">https://github.com/ksphp/webmis</a></p>
-			<p><b>下载地址:</b>&nbsp;&nbsp;<a href="https://github.com/ksphp/webmis-version">https://github.com/ksphp/webmis-version</a></p>
 			<p><b>QQ群:</b>&nbsp;&nbsp;206642028</p>
 		</td>
 	</tr>
