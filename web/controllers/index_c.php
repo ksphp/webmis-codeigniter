51aa50424d1920c69b2955adf27e60fc3189176f
kingsoul <admin@ksphp.com>
2015-04-16 14:46:23 +0800

    del

--- web/controllers/index_c.php
+++ web/controllers/index_c.php
@@ -1,7 +1,6 @@
 <?php
 class Index_c extends CI_Controller {
 	public function index(){
-		echo md5('kingsoul.ksphp.com');
 		$this->load->library('inc');
 		$data['Menus'] = $this->inc->getMenuUser($this);	//WEB分类菜单
 		$data['css'] = array('index.css');
