5a44cafc405ea21738f7a49fff035be510fd6778
kingsoul <admin@ksphp.com>
2015-04-21 12:24:13 +0800

    key

--- themes/web/default/inc/web_bottom.php
+++ themes/web/default/inc/web_bottom.php
@@ -1,9 +1,7 @@
 
-	</div>
 	<div class="web_copy">
 		Copyright © <a href="http://webmis.ksphp.com/admin" id="webmisVersion">ksphp.com</a> Tencent. All Rights Reserved
 	</div>
-	<!-- Index End -->
 	<div id="base_url" style="display: none;"><?php echo base_url().$this->config->config['index_url']; ?></div>
 	<script language="javascript" src="<?php echo base_url('../webmis/plugin/jquery/jquery-2.min.js'); ?>"></script>
 	<!--[if lte IE 9]>
